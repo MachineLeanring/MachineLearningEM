7ecbab41b54160b3acf1b9d5e812fedd7acbbd06
William-Hai <return_zero0@163.com>
2016-06-27 18:01:40 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MachineLearningEM
 数据挖掘之 EM 算法
+
+------------------------------------------
+
+本人CSDN博客 [点击链接](http://blog.csdn.net/lemon_tree12138)
---
+++ src/org/machine/learning/em/core/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <p></p>
+ * Create Date: 2016年6月27日
+ * Last Modify: 2016年6月27日
+ * 
+ * @author <a href="http://weibo.com/u/5131020927">Q-WHai</a>
+ * @see <a href="http://blog.csdn.net/lemon_tree12138">http://blog.csdn.net/lemon_tree12138</a>
+ * @version 0.0.1
+ */
+package org.machine.learning.em.core;
